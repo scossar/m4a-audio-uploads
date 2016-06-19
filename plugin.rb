abbbb7e0dcd058c41e56fce96083c642d2f2cc1c
scossar <scossar3@gmail.com>
2016-06-18 23:56:05 -0700

    override onebox

--- plugin.rb
+++ plugin.rb
@@ -4,3 +4,18 @@
 # url: https://github.com/scossar/m4a-audio-uploads
 
 register_asset 'javascripts/discourse/lib/utilities.js'
+
+class Onebox::Engine::AudioOnebox
+  include Onebox::Engine
+
+  matches_regexp /^(https?:)?\/\/.*\.(mp3|ogg|wav|m4a)(\?.*)?$/
+
+  def always_https?
+    WhitelistedGenericOnebox.host_matches(uri, WhitelistedGenericOnebox.https_hosts)
+  end
+
+  def to_html
+    "<audio controls><source src='#{@url}'><a href='#{@url}'>#{@url}</a></audio>"
+  end
+
+end
