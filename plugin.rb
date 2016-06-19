dbe2bb48a00e69086adb965061c78d7c28f5af81
scossar <scossar3@gmail.com>
2016-06-18 20:06:22 -0700

    add m4a to audio file list

--- plugin.rb
+++ plugin.rb
@@ -1,4 +1,6 @@
 # name: m4a audio uploads
 # about: adds m4a filetype to audio filetype list
 # authors: scossar
-# url: 
+# url: https://github.com/scossar/m4a-audio-uploads
+
+register_asset 'javascripts/discourse/lib/utilities.js'
